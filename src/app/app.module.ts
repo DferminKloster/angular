1c94cd2131901e9b345ab48639ae329da99f2197
Fermin Kloster <ferminkloster4@gmail.com>
2022-09-30 11:50:39 -0300

    agregue lo del carrito de compras

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GameListComponent } from './game-list/game-list.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GameListComponent
+    GameListComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
