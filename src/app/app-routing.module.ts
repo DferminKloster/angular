eb5f3242e0f3e21852ebbb6b221bda1abeb6432a
Fermin Kloster <ferminkloster4@gmail.com>
2022-10-07 20:41:10 -0300

    agregue routing a la aplicacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VideojuegoGamesComponent } from './videojuego-games/videojuego-games.component';
+import { VideojuegosAboutComponent } from './videojuegos-about/videojuegos-about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'games',
+    pathMatch:'full'
+  },
+  {
+    path: 'games',
+    component:VideojuegoGamesComponent
+  },
+  {
+    path: 'about',
+    component:VideojuegosAboutComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
